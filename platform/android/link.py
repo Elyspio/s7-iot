c7237388e271773f09b1d52f52fc32af047388c1
Jonathan Guichard <jona.guich69@gmail.com>
2020-11-12 15:06:34 +0100

    Template

---
+++ platform/android/link.py
@@ -0,0 +1,4 @@
+# code pour le lien entre les microbit et la platforme
+
+def run_android_link():
+    pass
---
+++ platform/microbit/link.py
@@ -0,0 +1,4 @@
+# code pour le lien entre l'application android et la platforme+
+
+def run_microbit_link():
+    pass
