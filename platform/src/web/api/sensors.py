fda9b33804554d2ae63a7edefbb421c9ef96357e
julesgnd <46670711+julesgnd@users.noreply.github.com>
2020-11-24 15:54:17 +0100

    POST_REQUEST
    correctif_post

--- platform/src/web/api/sensors.py
+++ platform/src/web/api/sensors.py
@@ -1,7 +1,12 @@
 from src.database import service
 from ..server import app
-
+from src.core.event import user_request_manager
+from flask import request
 
 @app.route("/api/sensors")
 def get_sensors():
     return service.to_json(service.get_sensors())
+
+@app.route("/api/sensors/order", methods=["POST"])
+def sensor_order():
+    user_request_manager.notify(request.form)
